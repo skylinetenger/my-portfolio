ea55e609fb1efdd7ea30d9d42e691364add4a1c2
tenger <skyline.mn12@gmail.com>
2018-08-13 16:40:01 +0800

    Menu Button && JavaScript && ROtate

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,36 +1,53 @@
 import React, { Component } from "react";
 
 class Navbar extends Component {
+  state = {
+    isShow: false
+  };
+
+  toggleMenu = () => {
+    this.setState({
+      isShow: !this.state.isShow
+    });
+  };
+
   render() {
     return (
       <header>
-        <div className="menu-btn">
-          <div className="menu-btn" />
-          <div className="menu-btn" />
-          <div className="menu-btn" />
+        <div
+          className={this.state.isShow ? "menu-btn close" : "menu-btn"}
+          onClick={this.toggleMenu}
+        >
+          <div className="btn-line" />
+          <div className="btn-line" />
+          <div className="btn-line" />
         </div>
 
-        <nav className="menu">
-          <div className="menu-branding">
+        <nav className={this.state.isShow ? "menu show" : "menu"}>
+          <div
+            className={
+              this.state.isShow ? "menu-branding show" : "menu-branding"
+            }
+          >
             <div className="portrait" />
           </div>
-          <ul className="menu-nav">
-            <li className="nav-item">
+          <ul className={this.state.isShow ? "menu-nav show" : "menu-nav"}>
+            <li className={this.state.isShow ? "nav-item show" : "nav-item"}>
               <a href="/" className="nav-link">
                 Home
               </a>
             </li>
-            <li className="nav-item">
+            <li className={this.state.isShow ? "nav-item show" : "nav-item"}>
               <a href="/about" className="nav-link">
                 About
               </a>
             </li>
-            <li className="nav-item">
+            <li className={this.state.isShow ? "nav-item show" : "nav-item"}>
               <a href="/work" className="nav-link">
                 Work
               </a>
             </li>
-            <li className="nav-item">
+            <li className={this.state.isShow ? "nav-item show" : "nav-item"}>
               <a href="/contact" className="nav-link">
                 How To Reach Me
               </a>
