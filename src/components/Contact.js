3d7ac2cb2f33700bef224f99d4ab5af8a214f821
tenger <skyline.mn12@gmail.com>
2018-08-14 17:34:30 +0800

    title and contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -10,14 +10,14 @@ class Contact extends Component {
         <h2 className="sm-heading">This is how you can reach me...</h2>
         <div className="boxes">
           <div>
-            <span className="text-secondary">Email:</span>
-            Skyline.mn12@gmail.com
+            <span className="text-secondary">Email: </span>
+            skyline.mn12@gmail.com
           </div>
           <div>
-            <span className="text-secondary">Phone:</span> (+976)86602215
+            <span className="text-secondary">Phone: </span> (+976)86602215
           </div>
           <div>
-            <span className="text-secondary">Address:</span> BZ District 14th
+            <span className="text-secondary">Address: </span> BZ District 14th
             Khoroo 81th Apt, UB, MN
           </div>
         </div>
